5fae56f0b9a3a17f8d6a79ba888c08d0e79de2ea
Gazntyno <gazntyno404@gmail.com>
2024-09-07 22:23:31 -0400

    added player movement
    added simple player movement.
    need to work on collisions and gravity

--- sandbox.js
+++ sandbox.js
@@ -32,16 +32,7 @@ scene.add(ambLight);
 
 const clock = new THREE.Clock();
 
-//setup controls
 const controls = new PointerLockControls(camera, document.body);
-
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-
-const velocity = new THREE.Vector3();
-const direction = new THREE.Vector3();
 
 //event listener for pointer lock
 document.addEventListener(
@@ -51,74 +42,6 @@ document.addEventListener(
   },
   false
 );
-
-//add event listener for keydown
-window.addEventListener("keydown", (event) => {
-  switch (event.code) {
-    case "KeyW":
-      moveForward = true;
-      break;
-    case "KeyS":
-      moveBackward = true;
-      break;
-    case "KeyA":
-      moveLeft = true;
-      break;
-    case "KeyD":
-      moveRight = true;
-      break;
-    case "Escape":
-      controls.unlock();
-      break;
-  }
-});
-
-//event listener for keydown
-/*document.addEventListener(
-  "keydown",
-  (event) => {
-    switch (event.code) {
-      case "KeyW":
-        moveForward = true;
-        break;
-      case "KeyS":
-        moveBackward = true;
-        break;
-      case "KeyA":
-        moveLeft = true;
-        break;
-      case "KeyD":
-        moveRight = true;
-        break;
-      case "Escape":
-        controls.unlock();
-        break;
-    }
-  },
-  false
-);
-
-//event listener for keyup
-document.addEventListener(
-  "keyup",
-  (event) => {
-    switch (event.code) {
-      case "KeyW":
-        moveForward = false;
-        break;
-      case "KeyS":
-        moveBackward = false;
-        break;
-      case "KeyA":
-        moveLeft = false;
-        break;
-      case "KeyD":
-        moveRight = false;
-        break;
-    }
-  },
-  false
-);*/
 
 //handle pointer lock state changes
 controls.addEventListener("lock", () => {
@@ -154,31 +77,60 @@ class Box extends THREE.Mesh {
 
     this.position.set(position.x, position.y, position.z);
 
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
+
     this.bottom - this.position.y - this.height / 2;
     this.top = this.position.y + this.height / 2;
 
-    this.velocity = velocity;
-    this.gravity = -0.003;
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
 
+    this.velocity = velocity;
+    this.gravity = -0.002;
   }
-  update(ground) {
+
+  updateSides() {
     this.bottom = this.position.y - this.height / 2;
     this.top = this.position.y + this.height / 2;
 
-    this.velocity.y += this.gravity;
+    this.right = this.position.x + this.width / 2;
+    this.left = this.position.x - this.width / 2;
 
-    //this is the ground collision
-   this.applyGravity();
+    this.front = this.position.z + this.depth / 2;
+    this.back = this.position.z - this.depth / 2;
   }
 
-  applyGravity() {
-    if(this.bottom + this.velocity.y <= ground.top){
+  update(ground) {
+    this.updateSides();
+
+    this.position.x += this.velocity.x;
+    this.position.z += this.velocity.z;
+
+    //this is the ground collision
+    this.applyGravity(ground);
+  }
+
+  applyGravity(ground) {
+    this.velocity.y += this.gravity;
+
+    if (this.bottom + this.velocity.y <= ground.top) {
       this.velocity.y *= 0.8;
       this.velocity.y = -this.velocity.y;
     } else {
       this.position.y += this.velocity.y;
     }
   }
+}
+
+function boxCollision({ box1, box2 }) {
+  //detect collision
+  const xCollision = box1.right >= box2.left && box1.left <= box2.right;
+  const yCollision =
+    box1.bottom + box1.velocity.y <= box2.top && box1.top >= box2.bottom;
+  const zCollision = box1.front >= box2.back && box1.back <= box2.front;
+
+  return xCollision && yCollision && zCollision;
 }
 
 //add cube
@@ -203,39 +155,81 @@ const ground = new Box({
 ground.receiveShadow = true;
 scene.add(ground);
 
+//setup controls
+
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+};
+
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      keys.w.pressed = true;
+      break;
+    case "KeyS":
+      keys.s.pressed = true;
+      break;
+    case "KeyA":
+      keys.a.pressed = true;
+      break;
+    case "KeyD":
+      keys.d.pressed = true;
+      break;
+    case "Escape":
+      controls.unlock();
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      keys.w.pressed = false;
+      break;
+    case "KeyS":
+      keys.s.pressed = false;
+      break;
+    case "KeyA":
+      keys.a.pressed = false;
+      break;
+    case "KeyD":
+      keys.d.pressed = false;
+      break;
+    case "Escape":
+      controls.unlock();
+      break;
+  }
+});
+
 //add animation
 function animate() {
   requestAnimationFrame(animate);
 
   /*cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;*/
-  cube.update(ground);
-
-  //calculate delta for smooth movement
-  const delta = clock.getDelta();
 
   //movement settings
-  const accelerationFactor = 10;
-  const dampingFactor = 10.0;
-  const speedMultiplier = 40.0;
+  let playerSpeed = 0.03;
+  cube.velocity.x = 0;
+  cube.velocity.z = 0;
+  if (keys.a.pressed) cube.velocity.x = -playerSpeed;
+  else if (keys.d.pressed) cube.velocity.x = playerSpeed;
 
-  //update velocity
-  velocity.x -= velocity.x * dampingFactor * delta;
-  velocity.z -= velocity.z * dampingFactor * delta;
+  if (keys.w.pressed) cube.velocity.z = -playerSpeed;
+  else if (keys.s.pressed) cube.velocity.z = playerSpeed;
 
-  //update direction based on input
-  direction.z = Number(moveForward) - Number(moveBackward);
-  direction.x = Number(moveRight) - Number(moveLeft);
-  direction.normalize(); // this ensures consistent movements in all directions
-
-  //update velocity
-  if (moveForward || moveBackward)
-    velocity.z -= direction.z * accelerationFactor * speedMultiplier * delta;
-  if (moveLeft || moveRight)
-    velocity.x -= direction.x * accelerationFactor * speedMultiplier * delta;
-
-  controls.moveRight(-velocity.x * delta);
-  controls.moveForward(-velocity.z * delta);
+  cube.update(ground);
 
   renderer.render(scene, camera);
 }
